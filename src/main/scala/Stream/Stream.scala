38e6da1808157113abf75d282936006337b14e2f
AlexStuckless <stuckless.a@gmail.com>
2016-08-05 23:14:18 -0700

    couple more stream exercises

--- src/main/scala/Stream/Stream.scala
+++ src/main/scala/Stream/Stream.scala
@@ -3,31 +3,44 @@ package Stream
 import Option._
 
 case object Empty extends Stream[Nothing]
-case class cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 trait Stream[+A]{
+  import Stream._
+
   def headOption: Option[A] = this match {
     case Empty => None
-    case cons(h, t) => Some(h())
+    case Cons(h, t) => Some(h())
   }
 
   def toList: List[A] = this match {
-    case cons(h, t:Stream[A]) => h() :: t.toList
+    case Cons(h, t:Stream[A]) => h() :: t.toList
     case Empty => Nil
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h,t) if n > 0 => cons(h(), t().take(n-1))
+    case _ => Empty
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h,t) if n > 0 => t().drop(n-1)
+    case Cons(h,t) => cons(h(),t())
+    case _ => Empty
   }
 
 }
 
 object Stream {
-  def Cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
     lazy val head = hd
     lazy val tail = tl
-    cons(() => head, () => tail)
+    Cons(() => head, () => tail)
   }
 
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty else Cons(as.head, apply(as.tail: _*))
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 }
 
